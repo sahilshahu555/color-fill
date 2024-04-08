8147233ab1fb31cf2c72346ff00e53ec6b156a5b
sahilshahu555 <sahilshahu555@gmail.com>
2024-04-08 17:23:20 +0530

    craeted navbar and Footer

---
+++ components/Footer.tsx
@@ -0,0 +1,74 @@
+import Link from "next/link"
+import Image from "next/image"
+import { FaFacebook, FaTwitter ,FaInstagram ,FaLinkedin  } from "react-icons/fa";
+const Footer = () => {
+  return (
+    <div className=' bg-[#0F172A]' id='FOOTER'>
+
+        <div className='flex flex-col md:flex-row md:items-start px-10 md:px-0 justify-around gap-10 md:gap-3     text-[#707581] py-14' >
+            
+            <div className="divF1 flex flex-col gap-1">
+              <h1 className="text-white font-semibold mb-1">Resources</h1>
+              <Link href="/">API & documentation</Link>
+              <Link href="/">Blog</Link>
+              <Link href="/">Use cases</Link>
+              <Link href="/">Platform status</Link>
+            </div>
+
+            <div className="divF2 flex flex-col gap-1">
+              <h1 className="text-white font-semibold mb-1">Company</h1>
+              <Link href="/">Privacy policy</Link>
+              <Link href="/">Terms and condition</Link>
+              <Link href="/">About us</Link>
+              <Link href="/">Contact us</Link>
+            </div>
+           
+            <div className="divF3 flex flex-col gap-1">
+
+              <div className="LOGO flex   gap-x-2">
+                <Image src="/images/logo.png"  alt="logo" className="w-5 h-5 text-white" width={50} height={50}/>
+                <h1 className="text-white font-semibold ">Colorfill.co</h1>
+              </div>
+
+              <h1 className="text-white font-semibold mb-3 mt-4 md:mt-8">Subscribe to our Newsletter</h1>
+
+              <div className="flex md:items-center items-left justify-center ">
+                <div >
+                  <input type="text" placeholder="Enter your Email" className="bg-[#2B2E3C] px-3 py-3 text-xs" />
+                </div>
+                <button className="text-black bg-white rounded-r-lg px-6 py-3 text-xs font-semibold active:scale-95">Subscribe</button>
+              </div>
+
+            </div>
+            
+        </div>
+
+        <div className='text-center  relative pb-5 '>
+
+           <div className="absolute left-[25%] md:left-[37%] -top-2 bg-[#0F172A] px-5 flex flex-col items-center justify-center gap-5 text-[#707581]">
+
+            <div className="flex items-center gap-5 "> 
+              <p className="   text-xs ">© Copyright colorfill.co 2024</p>
+              <Link href="/" className="hidden md:block"><FaFacebook /></Link>
+              <Link href="/" className="hidden md:block"><FaTwitter  /></Link>
+              <Link href="/" className="hidden md:block"><FaInstagram /></Link>
+              <Link href="/" className="hidden md:block"><FaLinkedin /></Link>
+            </div>
+             <div className="flex  juatify-around gap-4 md:hidden ">
+                <Link href="/"><FaFacebook /></Link>
+                <Link href="/"><FaTwitter /></Link>
+                <Link href="/"><FaInstagram /></Link>
+                <Link href="/"><FaLinkedin /></Link>
+             </div>
+
+           </div>
+
+           <div className="  w-full bg-[#707581] h-[0.5px] mb-10"></div>
+
+        </div>
+
+   </div>
+  )
+}
+
+export default Footer
---
+++ components/Navbar.tsx
@@ -0,0 +1,74 @@
+"use client"
+import Link from "next/link"
+import Image from "next/image"
+import {useState} from "react"
+const Navbar = () => {
+    const[menu,setMenu] = useState(false)
+  return (
+
+        <nav className="NAVBAR bg-gradient-to-l from-white via-[#F0F8FB] to-white text-black  flex items-center justify-around py-4 sticky top-0">
+            
+
+            <div className="LOGO hidden md:flex  items-center justify-center gap-0">
+                <Image src="/icons/Logo.svg"  alt="logo" className="w-10 h-10" width={50} height={50}/>
+            </div>
+
+            <div className="LAPTOP-VIEW hidden md:flex justify-center  items-center   ">
+               
+                <div className="flex gap-10 ">
+                    <Link className="link" href="/">What</Link>
+                    <Link className="link" href="/">Pricing</Link> 
+                    <Link className="link" href="/">How</Link>
+                    <Link className="link" href="/">Why</Link>
+                    <Link className="link" href="/">Contact</Link> 
+                </div>
+
+            </div>
+
+             <div className="Profile hidden md:flex justify-center gap-1 items-center">
+
+                <Link  href="/">
+                    <button className= " link1 bg-white text-black text-sm font-semibold px-6 py-2 rounded-2xl active:scale-95 text-left hover:bg-[#313852] hover:text-white">Login</button>
+                </Link>
+
+                <Link href="/">
+                    <button className= " link1 bg-black text-white text-sm font-semibold px-6 py-2 rounded-2xl active:scale-95 hover:bg-[#313852]">Signup</button>
+                </Link>
+            </div>
+
+            <div className="MOBILE-VIEW md:hidden  w-full  ">
+                <div className="flex  justify-around items-center p-2">
+                <Image src="/icons/Logo.svg"  alt="logo" className="w-10" width={50} height={50}/>
+                    <button className=" w-6" onClick={()=>{setMenu(!menu)}}>
+                        <svg  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/>
+                        </svg>
+                    </button>
+                </div>
+
+                <div className={` ${menu?"grid grid-cols-1 gap-3  pl-20 p-3 font-semibold ":"hidden"} ease-in duration-1000 `}>
+                    <Link className="link" href="/">What</Link>
+                    <Link className="link" href="/">Pricing</Link> 
+                    <Link className="link" href="/">How</Link>
+                    <Link className="link" href="/">Why</Link>
+                    <Link className="link" href="/">Contact</Link>
+
+                    <div className=" flex gap-10">
+                        <Link  href="/">
+                          <button className= " link1 bg-white text-black text-sm font-semibold px-6 py-2 rounded-2xl active:scale-95 text-left">Login</button>
+                        </Link>
+
+                        <Link href="/">
+                          <button className= " link1 bg-black text-white text-sm font-semibold px-6 py-2 rounded-2xl active:scale-95">Signup</button>
+                        </Link>
+                    </div>
+                    
+                </div>
+
+            </div>
+        </nav>
+
+    
+  )
+}
+
+export default Navbar
